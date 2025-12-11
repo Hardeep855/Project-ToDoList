feef6af49f94637abe4fe3caba7e6b605cb22286
chahalhardeep270 <chahalhardeep270@gmail.com>
2025-12-11 17:27:20 +0000

    update

---
+++ lib/screens/components/build_add_task_section.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget buildAddTaskSection(nameController, Function(String) addTask) {
+  return Container(
+    decoration: BoxDecoration(color: Colors.white),
+    child: Column(children: [
+      ],
+    
+    ),
+  );
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:myapp/services/task_service.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:myapp/providers/task_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:myapp/models/task_model.dart';
+import 'package:myapp/screens/components/build_task_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final nameController = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +43,15 @@ class _HomePageState extends State<HomePage> {
             focusedDay: DateTime.now(),
             firstDay: DateTime(2025),
             lastDay: DateTime(2027),
+          ),
+          Consumer<TaskProvider>(
+            builder: (context, taskProvider, child) {
+              return buildTaskList(
+                taskProvider.tasks,
+                taskProvider.removeTask,
+                taskProvider.updateTask,
+              );
+            },
           ),
         ],
       ),
